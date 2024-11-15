d8ba049950b567ca1bab019dcae42ac67f7fdfcd
zhouxingchi12345 <zhouxingchi12345@126.com>
2024-11-15 10:51:31 +0900

    完成了整个后端的开发

--- src/main/java/com/msr/controller/FileUploadController.java
+++ src/main/java/com/msr/controller/FileUploadController.java
@@ -1,12 +1,6 @@
 package com.msr.controller;
 
-import com.msr.pojo.entity.Result;
-import com.msr.utils.AliOssUtil;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.UUID;
 
 /**
  * @author wjing
@@ -15,7 +9,7 @@ import java.util.UUID;
  */
 @RestController
 public class FileUploadController {
-    @PostMapping("/upload")
+/*    @PostMapping("/upload")
     public Result<String> upload(MultipartFile file) throws Exception {
         //把文件的内容存储到本地磁盘上
         String originalFilename = file.getOriginalFilename();
@@ -23,5 +17,5 @@ public class FileUploadController {
         String filename = UUID.randomUUID().toString()+originalFilename.substring(originalFilename.lastIndexOf("."));
         String url = AliOssUtil.uploadFile(filename,file.getInputStream());
         return Result.success(url);
-    }
+    }*/
 }
